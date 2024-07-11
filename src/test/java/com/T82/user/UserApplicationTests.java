623a20764b1a4b4eedd1ae1164e66ea7d93a88ed
tjdaniel1 <robot2daniel@gmail.com>
2024-07-11 15:22:11 +0900

    Feat: 초기 환경 세팅

---
+++ src/test/java/com/T82/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.T82.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
