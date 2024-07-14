ca4121c56c7e2cf1dea014eb1a3e9d31842c69de
tjdaniel1 <robot2daniel@gmail.com>
2024-07-15 00:20:52 +0900

    Feat: 비밀번호 인코딩 기능 추가 #20 회원가입 시 입력한 비밀번호가 인코딩되어 DB에 저장되는 로직입니다.

---
+++ src/main/java/com/T82/user/global/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.T82.user.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+//    //JwtUtil 주입
+//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
+//    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.cors(c-> c.configurationSource(request -> {
+            var corsConfiguration = new CorsConfiguration();
+            corsConfiguration.addAllowedHeader("*");
+            corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","OPTIONS"));
+            corsConfiguration.setAllowedOrigins(List.of("*"));
+            return corsConfiguration;
+        }));
+//        http.authorizeHttpRequests(auth ->
+//                auth.requestMatchers("/actuator/**","/users/login", "/users/qrcode/**", "/users/signup","/api/gcs/**","/users/search/**","/users/change/password")
+//                        .permitAll()
+//                        .anyRequest()
+//                        .authenticated()
+//        );
+//        http.addFilterAt(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
